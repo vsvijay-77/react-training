64929dafc04229381e909c9676002e4051db1770
vsvijay-77 <vijay6432e@gmail.com>
2025-06-29 18:37:15 +0530

    a

--- social_app/src/App.js
+++ social_app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <p>TESTING</p>
+     <img src="bg-anime.png" height="300px"  width="300px"/>
     </div>
   );
 }
